2ae0534a7031673a9cd94043521926b58c426da1
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-12-22 21:55:23 -0800

    Update comment

--- lib/demiurge/createjs.rb
+++ lib/demiurge/createjs.rb
@@ -5,9 +5,9 @@ require "demiurge/createjs/websocket"
 require "demiurge/createjs/humanoid"
 require "demiurge/createjs/player"
 
-# Demiurge-Createjs adds display technology for an HTML game on top of
-# the Demiurge engine for game state. Despite the name, it includes
-# not only CreateJS but also Websocket, EventMachine and more.
+# Demiurge-Createjs adds display libraries for an HTML game on top of
+# the Demiurge engine for game state. It includes not only CreateJS
+# but also Websocket, EventMachine and more.
 
 module Demiurge
   module Createjs
